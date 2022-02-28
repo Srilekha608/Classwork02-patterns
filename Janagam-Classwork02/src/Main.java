43b704b44fa819d64ba2d8db0e4ff1501fc94e1a
Srilekha608 <64957724+Srilekha608@users.noreply.github.com>
2022-02-28 14:41:36 -0600

    Add files via upload

---
+++ Janagam-Classwork02/src/Main.java
@@ -0,0 +1,25 @@
+
+public class Main {
+	public static void main(String[] args)
+    {
+        BST tree = new BST();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+ 
+        System.out.println(
+            "Preorder traversal of binary tree is ");
+        tree.printPreorder();
+ 
+        System.out.println(
+            "\nInorder traversal of binary tree is ");
+        tree.printInorder();
+ 
+        System.out.println(
+            "\nPostorder traversal of binary tree is ");
+        tree.printPostorder();
+    }
+}
+
